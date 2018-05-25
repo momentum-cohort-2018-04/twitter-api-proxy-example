2f1711485e4d27ba1f490f6aa794ae25eca89424
Clinton Dreisbach <clinton@dreisbach.us>
2018-05-25 12:41:14 -0400

    Added Genius proxy

---
+++ server.js
@@ -0,0 +1,10 @@
+// var http = require('http')
+var httpProxy = require('http-proxy')
+//
+// Create your proxy server and set the target in the options.
+//
+httpProxy.createProxyServer({
+  changeOrigin: true,
+  target: 'https://api.genius.com/',
+  headers: {'Authorization': 'Bearer ZlLSmhS4-K3TlAOvIFbGig39rqxTIahXvisPoBLzjo0gnikXxtrFakbexl1lxRSi'}
+}).listen(8000)
